7df69d86698ae2ff31bec008a6ae0f57b1b6f9d2
Thitias <lopesmatheus642@gmail.com>
2024-04-06 12:58:05 -0300

    fix: code was lost

--- funcoes_graficos.py
+++ funcoes_graficos.py
@@ -1,55 +1,28 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def cria_histograma(dados, titulo = '', xlabel = '', ylabel = ''):
+def create_histogram(data, title = '', xlabel = '', ylabel = '', size = (8,4)):
     
-    sns.histplot(x=dados, bins=20, kde=True, color='#8e7cc3')
+    ax = sns.histplot(x=data, bins=20, kde=True, color='#8e7cc3')
 
-    plt.title(titulo)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.show()
+    ax.figure.set_size_inches(size)
+    ax.set_title(title , fontsize=14)
+    ax.set_xlabel(xlabel, fontsize=10)
+    ax.set_ylabel(ylabel, fontsize=10)
+    ax
 
+def plot_violin_boxplot(dados, titulo, xlabel, ylabel, cor='#8e7cc3'):
 
-def cria_boxplot(dados, titulo = '', xlabel = '', ylabel = ''):
+    fig, axs = plt.subplots(1, 2, figsize=(12, 8))
 
-    sns.boxplot(y=dados, color='#8e7cc3')
-
-    plt.title(titulo)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.show()
-
-def cria_pairplot(dados, hue=None):
-
-    if hue is None:
-        sns.pairplot(dados,
-                kind='reg',
-                plot_kws={'ci': None, 
-                        'scatter_kws': {'color': '#ADD8E6', 's': 3}})
-    else:
-        sns.pairplot(dados, hue=hue,
-                     kind='reg',
-                     plot_kws={'ci': None, 
-                               'scatter_kws': {'color': '#ADD8E6', 's': 3}})
-    
-    plt.show()
-
-def plot_histogram_boxplot(dados, titulo, xlabel, ylabel, cor='#8e7cc3'):
-
-    fig, axs = plt.subplots(1, 2, figsize=(12, 5))
-
-
-    sns.histplot(x=dados, bins=20, kde=True, color=cor, ax=axs[0])
+    sns.boxplot(x=dados, color=cor, ax=axs[0])
     axs[0].set_title(titulo)
     axs[0].set_xlabel(xlabel)
     axs[0].set_ylabel(ylabel)
 
-
-    sns.boxplot(y=dados, color=cor, ax=axs[1])
+    sns.violinplot(x=dados,color=cor,linewidth=3)
     axs[1].set_xlabel(xlabel)
     axs[1].set_ylabel(ylabel)
-
 
     plt.tight_layout()
     plt.show()
