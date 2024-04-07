334c4ad41beac60e0a11ec1fe3ca2752212fb684
OtavioSotnas <otaviosotnas@gmail.com>
2024-04-07 19:28:27 -0300

    Revert "a"
    This reverts commit a627775078e30ad9dbd3bcbfc2fdc4b328dc14d2.

--- funcoes_graficos.py
+++ funcoes_graficos.py
@@ -3,7 +3,7 @@ import seaborn as sns
 
 def create_histogram(data, hue, title = '', xlabel = '', ylabel = '', size = (8,4)):
 
-    palette = {'completed': '#3f07e6', 'none': '#8e7cc3'}
+    palette = {'completed': '#8e7cc3', 'none': '#3f07e6'}
     ax = sns.histplot(x=data, bins=20, kde=True, hue=hue, palette=palette)
 
     ax.figure.set_size_inches(size)
@@ -12,16 +12,15 @@ def create_histogram(data, hue, title = '', xlabel = '', ylabel = '', size = (8,
     ax.set_ylabel(ylabel, fontsize=10)
     ax
 
-def create_violin_boxplot(data, hue, title, ylabel, color='#8e7cc3', size = (8,4)):
+def create_violin_boxplot(data, title, ylabel, color='#8e7cc3', size = (8,4)):
 
     fig, axs = plt.subplots(1, 2, figsize = size, sharey=True)
-    palette = {'completed': '#3f07e6', 'none': '#8e7cc3'}
 
-    sns.boxplot(data, hue=hue, color=color, linewidth=2, ax=axs[0], palette=palette)
+    sns.boxplot(data, color=color, linewidth=2, ax=axs[0])
     axs[0].set_title(title)
     axs[0].set_ylabel(ylabel, fontsize=11)
 
-    sns.violinplot(data,hue=hue, color=color,linewidth=3,palette=palette)
+    sns.violinplot(data,color=color,linewidth=3)
     axs[1].set_ylabel(ylabel, fontsize=11)
 
     plt.tight_layout()
